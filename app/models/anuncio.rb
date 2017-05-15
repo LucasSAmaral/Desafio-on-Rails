33b72f76733361848ba853ef152ca86af18edd3d
Lucas <lucassantosamaral@hotmail.com>
2017-05-15 00:15:28 +0000

    Upload de imagens para anúncio

--- app/models/anuncio.rb
+++ app/models/anuncio.rb
@@ -1,2 +1,8 @@
 class Anuncio < ApplicationRecord
+   has_attached_file :image,
+                  url: "/system/:hash.:extension",
+                  hash_secret: "abc123"
+    validates_attachment :image, presence: true,
+                     content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] },
+                     size: { in: 0..500.kilobytes }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'welcome/index'
 
   resources :imovel
+  
+  resources :anuncio
     
   root 'welcome#index'
 end
